078ee47c0d84e8e5ab6c0dc5ffee0a06b2c5822b
Shivam <B20135@students.iitmandi.ac.in>
2022-09-05 15:37:59 +0530

    1st Commit under bug fix

--- factorial.cpp
+++ factorial.cpp
@@ -6,8 +6,8 @@ int factorial(int n){
     int a;
     int b;
     if(n<0){
-        printf("%s","enter a positive number");
-        return 0;
+        // printf("%s","enter a positive number");
+        return -1;
     }
     if(n!=1){
 	return(n * factorial(n-1));
