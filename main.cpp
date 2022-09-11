1fbc5a0b3af97869a128eff5fac504b81cc2db2c
Shivam <B20135@students.iitmandi.ac.in>
2022-09-11 21:45:52 +0530

    Final commit

--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@ int main(){
     cout<<endl;
     cout<<"The factorial of 5 is ";
     cout<<factorial(5);
+    int x=56;
+    int y=98;
+    cout<<endl;
+    cout<<gcd(x,y);
+    cout<<endl;
+    cout<<gcd_rec(x,y);
     return 0;
 }
